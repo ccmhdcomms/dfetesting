e326fe3af250fd3bb6027b55bdd2e7e28dadb2c0
AIC eLearning <ccmhdcomms@gmail.com>
2026-09-18 13:06:07 +0800

    dfetesting

---
+++ story_content/5hrhZa8Erfa_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":640,"text":"Empirical evidence suggests that"},{"start":3048,"text":"PCC coupled with a supportive environment"},{"start":6336,"text":"leads to an increase in sensory and social engagement,"},{"start":10528,"text":"which in turn ameliorate functional and cognitive decline."}]}]};
+    window.globalLoadJsAsset('story_content/5hrhZa8Erfa_transcripts.js', JSON.stringify(data));
+})();
